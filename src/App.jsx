07ec5bb868f095688d36acfd9cab56e7839843f3
Vitor Dilucca <vitordilucca@hotmail.com>
2023-07-27 16:42:51 -0300

    sistema de pesquisa asincrono SPA com api do hack news legal pra crl

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,29 @@ import "./App.css";
 const App = () => {
   //state
   const [news, setNews] = useState([]);
+  const [searchQuery,setSearchQuery] = useState('react')
   //fetch news
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
   };
+
   useEffect(() => {
     fetchNews();
-  });
+  },[searchQuery]);
+
+  const handleChange=(e)=>{
+    setSearchQuery(e.target.value)
+  }
   return (
     <>
       <h2>News</h2>
+      <form action="">
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
       {news.map((n, i) => (
         <p key={i}>{n.title}</p>
         ))}
