d15b46606e9a26bbd27106a7cac77a829a78fe7f
Vitor Dilucca <vitordilucca@hotmail.com>
2023-07-27 17:36:27 -0300

    componentizando tudo p deixar mais simples supostamente xd

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ const App = () => {
   //state
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState("react");
-  const [url,setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react}')
-  const [loading,setLoading] = useState(false)
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=react}"
+  );
+  const [loading, setLoading] = useState(false);
   //fetch news
   const fetchNews = () => {
     //set loading to true
-    setLoading(true)
+    setLoading(true);
     fetch(url)
       .then((result) => result.json())
-      .then((data) => (setNews(data.hits),setLoading(false)))
+      .then((data) => (setNews(data.hits), setLoading(false)))
       .catch((error) => console.log(error));
   };
 
@@ -24,21 +26,31 @@ const App = () => {
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
   };
+
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   };
+
+  const showLoading = () => (loading ? <h2>Loading....</h2> : "");
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit} action="">
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button>Search</button>
+    </form>
+  );
+
+  const showNews = () => {
+    return news.map((n, i) => <p key={i}>{n.title}</p>);
+  };
+
   return (
     <>
       <h2>News</h2>
-      {loading? <h2>Loading....</h2>: ""}
-      <form onSubmit={handleSubmit} action="">
-        <input type="text" value={searchQuery} onChange={handleChange} />
-        <button>Search</button>
-      </form>
-      {news.map((n, i) => (
-        <p key={i}>{n.title}</p>
-      ))}
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </>
   );
 };
