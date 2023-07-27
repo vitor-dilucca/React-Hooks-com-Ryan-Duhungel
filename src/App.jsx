3c3a919809785c5b9cec9e40c5044883a6cb04f4
Vitor Dilucca <vitordilucca@hotmail.com>
2023-07-27 16:34:07 -0300

    fetching react news from hacknews api using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
 import React, { Component, useState, useEffect } from "react";
 import "./App.css";
 
-//functional component
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = `Clicked ${count} times`;
-  });
-
-  const increment = () => {
-    setCount(count + 1);
+  //state
+  const [news, setNews] = useState([]);
+  //fetch news
+  const fetchNews = () => {
+    fetch("http://hn.algolia.com/api/v1/search?query=react")
+      .then(result => result.json())
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
   };
-
+  useEffect(() => {
+    fetchNews();
+  });
   return (
     <>
-      <h2>Olá asa</h2>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
+      <h2>News</h2>
+      {news.map((n, i) => (
+        <p key={i}>{n.title}</p>
+        ))}
     </>
   );
 };
-
 export default App;
 
-//mesma coisa com class component
+//functional component, counter
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     document.title = `Clicked ${count} times`;
+//   });
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <>
+//       <h2>Olá asa</h2>
+//       <p>Count: {count}</p>
+//       <button onClick={increment}>Increment</button>
+//     </>
+//   );
+// };
+
+// export default App;
+
+//mesma coisa com class component, counter
 
 // class App extends Component {
 //   state = {
