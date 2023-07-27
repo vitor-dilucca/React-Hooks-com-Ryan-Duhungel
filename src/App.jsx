57ee37f0cd52e130cd795141ab270f8e87c1578c
Vitor Dilucca <vitordilucca@hotmail.com>
2023-07-27 17:21:12 -0300

    busca só é feita ao pressionar o botão

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,37 @@ import "./App.css";
 const App = () => {
   //state
   const [news, setNews] = useState([]);
-  const [searchQuery,setSearchQuery] = useState('react')
+  const [searchQuery, setSearchQuery] = useState("react");
+  const [url,setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react}')
   //fetch news
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
-      .then(result => result.json())
-      .then(data => setNews(data.hits))
-      .catch(error => console.log(error));
+    fetch(url)
+      .then((result) => result.json())
+      .then((data) => setNews(data.hits))
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
     fetchNews();
-  },[searchQuery]);
+  }, [url]);
 
-  const handleChange=(e)=>{
-    setSearchQuery(e.target.value)
-  }
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  };
   return (
     <>
       <h2>News</h2>
-      <form action="">
+      <form onSubmit={handleSubmit} action="">
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
       </form>
       {news.map((n, i) => (
         <p key={i}>{n.title}</p>
-        ))}
+      ))}
     </>
   );
 };
